b78aa8fd172f05eaa6a21727e26a2b6678c48cc1
Henrique Vale <henriquedovalefac@gmail.com>
2024-09-29 20:48:37 +0100

    added encryption, limit of peers comunicating set to 1, and other small changes

--- p2p-messaging-app/src/main/java/com/p2pmessagingapp/Peer.java
+++ p2p-messaging-app/src/main/java/com/p2pmessagingapp/Peer.java
@@ -5,8 +5,9 @@
  *  fazer é com que todos os users tenham permissão para falar com todos os outros. E outra coisa é que neste
  *  momento é possível uma mensagem enviada por um user ir para vários outros user e não um só, também temos de 
  *  mudar isso.
+ * tentar meter em SSL eu tentei mas estava meio dificil 
+ * 
  */
-
 
 
 package com.p2pmessagingapp;
@@ -20,25 +21,172 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.net.Socket;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import javax.crypto.Cipher;
 
+import javax.xml.bind.DatatypeConverter;
 import javax.json.Json;
 
 public class Peer {
-    
+
+    byte[] otherPeerPubKey;
+    static KeyPair keys;
     public static void main(String[] args) throws Exception {
 
         String[] values;
         ServerThread serverThread;
-        String errorMessage;
-        
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("=> Please enter your id & port below:");
+        BufferedReader bufferedReader;
                 
-        while (true) { 
+        while (true) {
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("=> Please enter your id & port below:");
             values = bufferedReader.readLine().split(" "); // Reads and splits user input details
+            boolean verificationStatus = fileAndPortVerification(values);
+            if (verificationStatus) {
+                break;
+            }
+        }
+        serverThread = new ServerThread(values[1]);
+        serverThread.start();
+        keys = generateKeys();
+        System.out.println(keys.getPublic().getFormat()+"format " + keys.getPublic().getAlgorithm()+ " ALgo");
+        createClientFile(values[0], values[1],keys.getPublic().getEncoded());
 
-            errorMessage = "";
+        new Peer().updateListenToPeers(bufferedReader,values[0], serverThread);
+    }
+       
+    
+    
+    public void updateListenToPeers(BufferedReader bufferedReader, String id, ServerThread serverThread) throws Exception {
+        boolean validConnection= false;
+        String input=null;
+        String[] inputValues=null;
+        PeerThread peer=null;
+        while (!validConnection) {
+            System.out.println("Enter localhost:port");
+            System.out.println(" peers to receive messages from(s to skip):");
+            input = bufferedReader.readLine();
+            long count = input.chars().filter(c -> c ==':').count();
+            if (count>1) {
+                System.out.println("=> error: you can only connect to one user pls try again");
+            }
+            else{
+                inputValues = input.split(" "); // Split input by spaces into host:port pairs
+                validConnection=true;
+            }
+        }
+        
+        if(!input.equals("s")) {
+
+            for (int i = 0; i < inputValues.length; i++) {
+                String[] address = inputValues[i].split(":");
+                Socket socket = null;
+
+                try {  // Create a socket and start a PeerThread for communication
+                    socket = new Socket(address[0], Integer.valueOf(address[1]));
+                    peer = new PeerThread(socket,keys.getPrivate());
+                    peer.start(); // Initiates a PeerThread for communication with the connected peer
+
+                } catch (Exception e) { // Close the socket if it exists; print "invalid input" if not.
+                    if (socket != null) socket.close();
+                    else System.out.println("invalid input");
+                }
+            }
+        }
+        // Handles user communication with connected peers.
+        communicate(bufferedReader, id, serverThread, peer);
+    }
+    
+
+
+    /**
+    * Facilitates communication with peers by reading user input and sending messages.
+    *
+    * @param bufferedReader The BufferedReader to read user input from the console.
+    * @param id The unique identifier of the user (peer).
+    * @param serverThread The ServerThread instance responsible for sending messages to peers.
+    */
+
+    public void communicate(BufferedReader bufferedReader, String id, ServerThread serverThread,PeerThread peer) {
+
+        try {
+            System.out.println("You can now communicate (e to exit, c to change,k to send pubKey)");
+            boolean flag = true;
+            boolean pubKeySent= false;
+            while(flag) {
+
+                String message = bufferedReader.readLine();
+                
+                if(message.equals("e")) { //exit the communication
+                    flag = false;
+                    break;
+
+                } else if (message.equals("c")) { //change the peer
+                    updateListenToPeers(bufferedReader, id, serverThread);
+               
+                } 
+                else if (message.equals("k")){
+                    sendPubKey(id, serverThread);
+                    pubKeySent=true;
+                }    else {
+                    if (pubKeySent) {
+                        System.out.println(peer.getReceiverPKey().toString());
+                        byte[] receiverPKey= peer.getReceiverPKey();
+                        StringWriter stringWriter = new StringWriter();
+                        message = bytesToString(encryptRSA(message, receiverPKey));
+                    // Creates a JSON object with the user's ID and message for transmission.
+                    Json.createWriter(stringWriter).writeObject(Json.createObjectBuilder() 
+                                                .add("id", id)
+                                                .add("message", message)
+                                                .build());
+                    serverThread.sendMessage(stringWriter.toString());
+                    }
+                    else{
+                        System.out.println("=>error you have to send your pubkey before sending any messages");
+                    }
+                }
+            }
+            System.exit(0);
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    private static void createClientFile(String id, String port, byte[] key) {
+        try {
+            File dir = new File("clients");
+            if (!dir.exists()) {
+                dir.mkdir(); // Criar a diretoria se não existir
+            }
+
+            File file = new File(dir, id);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(port); // Escrever a porta no ficheiro
+            writer.write(System.getProperty("line.separator"));
+            writer.write(DatatypeConverter.printHexBinary(key));
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static KeyPair generateKeys()
+        throws Exception
+        {
+            SecureRandom secureRandom = new SecureRandom();
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048,secureRandom);
+            return keyPairGenerator.generateKeyPair();
+        }
+
+    private static boolean fileAndPortVerification( String[] values){
+        String errorMessage;
+        errorMessage = "";
             File filename = new File("clients/" + values[0]);
             if(filename.exists()) errorMessage = "ID";  
 
@@ -61,106 +209,41 @@ public class Peer {
                     }
                 }
             }
-        
-            if(errorMessage.equals("")) break;
-
+            if(errorMessage.equals("")) return true;
             System.out.println("=> " + errorMessage + " already in use, please insert a different " + errorMessage + ":");
-
-        }
-        serverThread = new ServerThread(values[1]);
-        serverThread.start();
-        createClientFile(values[0], values[1]);
-
-        new Peer().updateListenToPeers(bufferedReader,values[0], serverThread);
+            return false;
     }
-       
-    
-    
-    public void updateListenToPeers(BufferedReader bufferedReader, String id, ServerThread serverThread) throws Exception {
+    private static void sendPubKey(String id, ServerThread serverThread){
+        StringWriter stringWriter = new StringWriter();
+        byte[] pubKeytoSend = keys.getPublic().getEncoded();
+        String message = Base64.getEncoder().encodeToString(pubKeytoSend);          
+        // Creates a JSON object with the user's ID and message for transmission.
+        Json.createWriter(stringWriter).writeObject(Json.createObjectBuilder()
+                                    .add("id", id)
+                                    .add("key", message)
+                                    .build());
+        serverThread.sendMessage(stringWriter.toString());
+    } 
 
-        System.out.println("Enter hostname:port");
-        System.out.println(" peers to receive messages from(s to skip):");
-
-        String input = bufferedReader.readLine();
-        String[] inputValues = input.split(" "); // Split input by spaces into host:port pairs
-        
-        if(!input.equals("s")) {
-
-            for (int i = 0; i < inputValues.length; i++) {
-                String[] address = inputValues[i].split(":");
-                Socket socket = null;
-
-                try {  // Create a socket and start a PeerThread for communication
-                    socket = new Socket(address[0], Integer.valueOf(address[1]));
-                    new PeerThread(socket).start(); // Initiates a PeerThread for communication with the connected peer
-
-                } catch (Exception e) { // Close the socket if it exists; print "invalid input" if not.
-                    if (socket != null) socket.close();
-                    else System.out.println("invalid input");
-                }
-            }
-        }
-        // Handles user communication with connected peers.
-        communicate(bufferedReader, id, serverThread);
-    }
-    
-
-
-    /**
-    * Facilitates communication with peers by reading user input and sending messages.
-    *
-    * @param bufferedReader The BufferedReader to read user input from the console.
-    * @param id The unique identifier of the user (peer).
-    * @param serverThread The ServerThread instance responsible for sending messages to peers.
-    */
-
-    public void communicate(BufferedReader bufferedReader, String id, ServerThread serverThread) {
-
-        try {
-
-            System.out.println("You can now communicate (e to exit, c to change)");
-            boolean flag = true;
-            while(flag) {
-
-                String message = bufferedReader.readLine();
-
-                if(message.equals("e")) { //exit the communication
-                    flag = false;
-                    break;
-
-                } else if (message.equals("c")) { //change the peer
-                    updateListenToPeers(bufferedReader, id, serverThread);
-                    
-                } else {
-                    StringWriter stringWriter = new StringWriter();
-                    
-                    // Creates a JSON object with the user's ID and message for transmission.
-                    Json.createWriter(stringWriter).writeObject(Json.createObjectBuilder()
-                                                .add("id", id)
-                                                .add("message", message)
-                                                .build());
-                    serverThread.sendMessage(stringWriter.toString());
-                }
-            }
-            System.exit(0);
-        } catch (Exception e) { e.printStackTrace(); }
+    public static byte[] encryptRSA(String message, byte[] encodedPbKey) throws Exception {
+        PublicKey publicKey= byteToPublicKey(encodedPbKey); 
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(message.getBytes());
     }
 
-    public static void createClientFile(String id, String port) {
-        try {
-            File dir = new File("clients");
-            if (!dir.exists()) {
-                dir.mkdir(); // Criar a diretoria se não existir
-            }
+    public String bytesToString(byte[] bytes){
+        return Base64.getEncoder().encodeToString(bytes);
+    }
 
-            File file = new File(dir, id);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(port); // Escrever a porta no ficheiro
-            writer.close();
+    public byte[] stringToBytes(String string){
+        return Base64.getDecoder().decode(string);
+    }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static PublicKey byteToPublicKey(byte[] encodedPbKey) throws Exception {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPbKey);
+        return keyFactory.generatePublic(publicKeySpec);
     }
 }
 
--- p2p-messaging-app/src/main/java/com/p2pmessagingapp/PeerThread.java
+++ p2p-messaging-app/src/main/java/com/p2pmessagingapp/PeerThread.java
@@ -4,27 +4,53 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.security.PrivateKey;
+import java.util.Base64;
 
+import javax.crypto.Cipher;
 import javax.json.Json;
 import javax.json.JsonObject;
 
 
 public class PeerThread extends Thread {
     private BufferedReader bufferedReader;
-    public PeerThread(Socket socket) throws IOException {
+    private byte[] receiverPKey;
+    private PrivateKey privateKey;
+    public PeerThread(Socket socket,PrivateKey pKey) throws IOException {
         bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.privateKey= pKey;
     }
     public void run() {
         boolean flag = true;
         while(flag) {
             try {
                 JsonObject jsonObject = Json.createReader(bufferedReader).readObject();
-                if(jsonObject.containsKey("id"))
-                    System.out.println("["+jsonObject.getString("id")+"]: "+jsonObject.getString("message"));
+                if (jsonObject.containsKey("id") && jsonObject.containsKey("key")) {
+                    System.out.println(jsonObject.getString("id")+" key was recieved");
+                    receiverPKey = Base64.getDecoder().decode(jsonObject.getString("key"));
+                }
+                else if(jsonObject.containsKey("id")){
+                    String cleanMessage = decryptRSA(stringToBytes(jsonObject.getString("message")), privateKey);
+                    System.out.println("["+jsonObject.getString("id")+"]: "+cleanMessage);
+                }
             } catch(Exception e) {
                 flag = false;
                 interrupt();
             }
         }
     }
+    public byte[] getReceiverPKey(){
+        return receiverPKey;
+    }
+    private static String decryptRSA(byte[] encryptedMessage, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(encryptedMessage);
+        return new String(decryptedBytes);
+    }
+
+    public byte[] stringToBytes(String string){
+        return Base64.getDecoder().decode(string);
+    }
+
 }
